a477cf60bd6b10b0cfae04305538ea85cf5915a1
PietariRisku1 <pietari.risku@metropolia.fi>
2025-04-03 09:56:36 +0300

    hei

---
+++ Moduuli 10/Tehtävä 10.2.py
@@ -0,0 +1,39 @@
+class Hissi:
+    def __init__(self, alin_kerros, ylin_kerros):
+        self.alin_kerros = alin_kerros
+        self.ylin_kerros = ylin_kerros
+        self.nykyinen_kerros = alin_kerros
+
+    def kerros_ylos(self):
+        if self.nykyinen_kerros < self.ylin_kerros:
+            self.nykyinen_kerros += 1
+            print(f"Hissi on nyt kerroksessa {self.nykyinen_kerros}")
+
+    def kerros_alas(self):
+        if self.nykyinen_kerros > self.alin_kerros:
+            self.nykyinen_kerros -= 1
+            print(f"Hissi on nyt kerroksessa {self.nykyinen_kerros}")
+
+    def siirry_kerrokseen(self, kerros):
+        while self.nykyinen_kerros < kerros:
+            self.kerros_ylos()
+        while self.nykyinen_kerros > kerros:
+            self.kerros_alas()
+
+
+class Talo:
+    def __init__(self, alin_kerros, ylin_kerros, hissien_lkm):
+        self.hissit = [Hissi(alin_kerros, ylin_kerros) for _ in range(hissien_lkm)]
+
+    def aja_hissia(self, hissin_numero, kohdekerros):
+        if 0 <= hissin_numero < len(self.hissit):
+            print(f"Ajetaan hissiä {hissin_numero} kerrokseen {kohdekerros}")
+            self.hissit[hissin_numero].siirry_kerrokseen(kohdekerros)
+        else:
+            print("Virheellinen hissinumero")
+
+talo = Talo(1, 10, 3)
+talo.aja_hissia(0, 5)
+talo.aja_hissia(1, 8)
+talo.aja_hissia(2, 2)
+talo.aja_hissia(0, 1)
---
+++ Moduuli 10/Tehtävä10.1.py
@@ -0,0 +1,26 @@
+class Hissi:
+    def __init__(self, alin_kerros, ylin_kerros):
+        self.alin_kerros = alin_kerros
+        self.ylin_kerros = ylin_kerros
+        self.nykyinen_kerros = alin_kerros
+
+    def kerros_ylos(self):
+        if self.nykyinen_kerros < self.ylin_kerros:
+            self.nykyinen_kerros += 1
+            print(f"Hissi on nyt kerroksessa {self.nykyinen_kerros}")
+
+    def kerros_alas(self):
+        if self.nykyinen_kerros > self.alin_kerros:
+            self.nykyinen_kerros -= 1
+            print(f"Hissi on nyt kerroksessa {self.nykyinen_kerros}")
+
+    def siirry_kerrokseen(self, kerros):
+        while self.nykyinen_kerros < kerros:
+            self.kerros_ylos()
+        while self.nykyinen_kerros > kerros:
+            self.kerros_alas()
+
+
+h = Hissi(1, 10)
+h.siirry_kerrokseen(5)
+h.siirry_kerrokseen(1)
